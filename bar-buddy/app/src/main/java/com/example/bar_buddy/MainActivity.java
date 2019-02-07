973efcff243bbb837894a246807e26ea4c19fc8c
ngleason7 <ngleason7@gmail.com>
2019-02-07 14:42:42 -0600

    Update MainActivity.java

--- bar-buddy/app/src/main/java/com/example/bar_buddy/MainActivity.java
+++ bar-buddy/app/src/main/java/com/example/bar_buddy/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.bar_buddy;
 
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,26 +17,5 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
-    }
-
-    private DrawerLayout mDrawerLayout;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-        ...
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
