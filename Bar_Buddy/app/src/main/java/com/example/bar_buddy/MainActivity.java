aa515174197f084f5d9373fb3232c38bdfda536b
ngleason7 <ngleason7@gmail.com>
2019-02-15 17:14:30 -0600

    PageView linked to Bottom Nav

--- Bar_Buddy/app/src/main/java/com/example/bar_buddy/MainActivity.java
+++ Bar_Buddy/app/src/main/java/com/example/bar_buddy/MainActivity.java
@@ -16,14 +16,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements HomeTab.OnFragmentInteractionListener,FavoritesTab.OnFragmentInteractionListener, UpdatesTab.OnFragmentInteractionListener {
 
-    private TextView mTextMessage;
     private DrawerLayout mDrawerLayout;
 
     ViewPager viewPager;
-    PagerAdapter adapter;
+    HomeTab homeTab;
+    FavoritesTab favoritesTab;
+    UpdatesTab updatesTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +34,10 @@ public class MainActivity extends AppCompatActivity implements HomeTab.OnFragmen
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        setupViewPager(viewPager);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -42,62 +45,7 @@ public class MainActivity extends AppCompatActivity implements HomeTab.OnFragmen
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
-        //final TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
 
-        /*int tabIconColor = ContextCompat.getColor(getApplicationContext(), R.color.tabUnselectedIconColor);
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_home));
-        //tabLayout.getTabAt(0).setText("Home");
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_favorite));
-        tabLayout.getTabAt(1).getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
-        //tabLayout.getTabAt(1).setText("Favorites");
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_updates));
-        tabLayout.getTabAt(2).getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
-        //tabLayout.getTabAt(2).setText("Updates");*/
-
-        /*View view1 = getLayoutInflater().inflate(R.layout.customtab, null);
-        view1.findViewById(R.id.icon).setBackgroundResource(R.drawable.ic_home);
-        tabLayout.addTab(tabLayout.newTab().setCustomView(view1));
-
-
-        View view2 = getLayoutInflater().inflate(R.layout.customtab, null);
-        view2.findViewById(R.id.icon).setBackgroundResource(R.drawable.ic_favorite);
-        tabLayout.addTab(tabLayout.newTab().setCustomView(view2));
-
-
-        View view3 = getLayoutInflater().inflate(R.layout.customtab, null);
-        view3.findViewById(R.id.icon).setBackgroundResource(R.drawable.ic_updates);
-        tabLayout.addTab(tabLayout.newTab().setCustomView(view3));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        viewPager = (ViewPager)findViewById(R.id.pager);
-        //tabLayout.setupWithViewPager(viewPager);
-        //setupViewPager();
-
-        adapter = new PagerAdapter(getSupportFragmentManager(), 3);
-        viewPager.setAdapter(adapter);
-        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        tabLayout.setOnTabSelectedListener(
-                new TabLayout.ViewPagerOnTabSelectedListener(viewPager) {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                super.onTabSelected(tab);
-                int tabIconColor = ContextCompat.getColor(getApplicationContext(), R.color.tabSelectedIconColor);
-                tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                super.onTabUnselected(tab);
-                int tabIconColor = ContextCompat.getColor(getApplicationContext(), R.color.tabUnselectedIconColor);
-                tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });*/
     }
 
     @Override
@@ -117,17 +65,16 @@ public class MainActivity extends AppCompatActivity implements HomeTab.OnFragmen
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    viewPager.setCurrentItem(1);
-
-                    mTextMessage.setText(R.string.title_home);
+                    viewPager.setCurrentItem(0, false);
+                    Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_dashboard:
-                    viewPager.setCurrentItem(2);
-                    mTextMessage.setText(R.string.title_dashboard);
+                    viewPager.setCurrentItem(1, false);
+                    Toast.makeText(getApplicationContext(), "Favs", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_notifications:
-                    viewPager.setCurrentItem(3);
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(2, false);
+                    Toast.makeText(getApplicationContext(), "Updates", Toast.LENGTH_SHORT).show();
                     return true;
             }
             return false;
@@ -139,4 +86,17 @@ public class MainActivity extends AppCompatActivity implements HomeTab.OnFragmen
     public void onFragmentInteraction(Uri uri) {
 
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        homeTab = new HomeTab();
+        favoritesTab = new FavoritesTab();
+        updatesTab = new UpdatesTab();
+        pagerAdapter.addFragment(homeTab);
+        pagerAdapter.addFragment(favoritesTab);
+        pagerAdapter.addFragment(updatesTab);
+        viewPager.setAdapter(pagerAdapter);
+    }
 }
+
+
--- Bar_Buddy/app/src/main/java/com/example/bar_buddy/PagerAdapter.java
+++ Bar_Buddy/app/src/main/java/com/example/bar_buddy/PagerAdapter.java
@@ -4,34 +4,29 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PagerAdapter extends FragmentStatePagerAdapter {
 
+    private final List<Fragment> mFragmentList = new ArrayList<>();
     int mNoOfTabs;
 
-    public PagerAdapter(FragmentManager fm, int NumberOfTabs) {
+    public PagerAdapter(FragmentManager fm) {
         super(fm);
-        this.mNoOfTabs = NumberOfTabs;
     }
 
     @Override
     public Fragment getItem(int position) {
-        switch(position) {
-            case 0:
-                HomeTab homeTab = new HomeTab();
-                return homeTab;
-            case 1:
-                FavoritesTab favoritesTab = new FavoritesTab();
-                return favoritesTab;
-            case 2:
-                UpdatesTab updatesTab = new UpdatesTab();
-                return updatesTab;
-            default:
-                return null;
-        }
+        return mFragmentList.get(position);
     }
 
     @Override
     public int getCount() {
-        return mNoOfTabs;
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
     }
 }
