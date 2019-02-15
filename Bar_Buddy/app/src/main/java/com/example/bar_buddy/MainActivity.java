9a811ae63ad3b05767291020c9bbfedc57bd9d23
ngleason7 <ngleason7@gmail.com>
2019-02-15 17:25:33 -0600

    Implementing Pager

--- Bar_Buddy/app/src/main/java/com/example/bar_buddy/MainActivity.java
+++ Bar_Buddy/app/src/main/java/com/example/bar_buddy/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,13 +16,17 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
     private DrawerLayout mDrawerLayout;
 
-    private Menu searchMenu;
+    ViewPager viewPager;
+    HomeTab homeTab;
+    FavoritesTab favoritesTab;
+    UpdatesTab updatesTab;
+    AccountTab accountTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        setupViewPager(viewPager);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -58,17 +64,30 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    viewPager.setCurrentItem(0, false);
+                    Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    viewPager.setCurrentItem(1, false);
+                    Toast.makeText(getApplicationContext(), "Favs", Toast.LENGTH_SHORT).show();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(2, false);
+                    Toast.makeText(getApplicationContext(), "Updates", Toast.LENGTH_SHORT).show();
                     return true;
             }
             return false;
         }
     };
 
+    private void setupViewPager(ViewPager viewPager) {
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        homeTab = new HomeTab();
+        favoritesTab = new FavoritesTab();
+        updatesTab = new UpdatesTab();
+        pagerAdapter.addFragment(homeTab);
+        pagerAdapter.addFragment(favoritesTab);
+        pagerAdapter.addFragment(updatesTab);
+        viewPager.setAdapter(pagerAdapter);
+    }
 }
