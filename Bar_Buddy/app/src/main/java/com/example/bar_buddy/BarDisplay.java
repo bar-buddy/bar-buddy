27e594e66f5b306e3c56526876878d9c40b422b9
swmaryland <swmaryland@crimson.ua.edu>
2019-03-09 16:21:14 -0500

    adding and reading data

--- Bar_Buddy/app/src/main/java/com/example/bar_buddy/BarDisplay.java
+++ Bar_Buddy/app/src/main/java/com/example/bar_buddy/BarDisplay.java
@@ -1,13 +1,54 @@
 package com.example.bar_buddy;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BarDisplay extends AppCompatActivity {
+
+    //FirebaseFirestore instance
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public BarDisplay() {
+        // Create a new bar
+        CollectionReference b = db.collection("bars");
+        b.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Map<String, Object> map = document.getData();
+                        String name = map.get("bar_name").toString();
+                        String address = map.get("bar_address").toString();
+                        String desc = map.get("bar_description").toString();
+                        String phone = map.get("bar_phone").toString();
+                        System.out.println(name);
+                        System.out.println(address);
+                        System.out.println(desc);
+                        System.out.println(phone);
+                    }
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
