b87ffa801a963f5d5f6018d29c96297ff5e7ed1c
ngleason7 <ngleason7@gmail.com>
2019-03-28 14:30:13 -0500

    Update BarItem.java

--- Bar_Buddy/app/src/main/java/com/example/bar_buddy/BarItem.java
+++ Bar_Buddy/app/src/main/java/com/example/bar_buddy/BarItem.java
@@ -1,18 +1,24 @@
 package com.example.bar_buddy;
 
 public class BarItem {
-    String uid;
-    String bar_name;
-    String bar_cover;
-    String bar_wait_time_minutes;
-    String bar_description;
+    private String bar_name;
+    private String bar_cover;
+    private String bar_wait_time_minutes;
+    private String bar_description;
+    private String bar_phone;
+    private String bar_address;
 
     public BarItem() {
 
     }
 
-    public BarItem(String text) {
-        this.bar_name = text;
+    public BarItem(String bar_name, String bar_cover, String bar_wait_time_minutes, String bar_description, String bar_phone, String bar_address) {
+        this.bar_name = bar_name;
+        this.bar_cover = bar_cover;
+        this.bar_wait_time_minutes = bar_wait_time_minutes;
+        this.bar_description = bar_description;
+        this.bar_phone = bar_phone;
+        this.bar_address = bar_address;
     }
 
     public String getBar_name() {
@@ -31,4 +37,11 @@ public class BarItem {
         return bar_description;
     }
 
+    public String getBar_phone() {
+        return bar_phone;
+    }
+
+    public String getBar_address() {
+        return bar_address;
+    }
 }
