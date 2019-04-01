f0c01fa6711f691c218aa68e4c1b93365600c48c
ngleason7 <ngleason7@gmail.com>
2019-04-01 13:04:36 -0500

    Update FavoritesTab.java

--- Bar_Buddy/app/src/main/java/com/example/bar_buddy/TabFragments/FavoritesTab.java
+++ Bar_Buddy/app/src/main/java/com/example/bar_buddy/TabFragments/FavoritesTab.java
@@ -3,7 +3,9 @@ package com.example.bar_buddy.TabFragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,31 +13,24 @@ import android.view.ViewGroup;
 import com.example.bar_buddy.Adapters.BarCardAdapter;
 import com.example.bar_buddy.AdapterItems.BarItem;
 import com.example.bar_buddy.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
 
+import static android.support.constraint.Constraints.TAG;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FavoritesTab.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FavoritesTab#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FavoritesTab extends Fragment {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference barsRef = db.collection("bars");
 
     private BarCardAdapter adapter;
     private List<BarItem> bars;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -43,31 +38,40 @@ public class FavoritesTab extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavoritesTab.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FavoritesTab newInstance(String param1, String param2) {
-        FavoritesTab fragment = new FavoritesTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+    }
+
+    private void readData(final FirestoreCallback firestoreCallback) {
+        barsRef
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+
+                                BarItem b = document.toObject(BarItem.class);
+                                b.setBar_id(document.getId());
+
+                                if(bars != null) {
+                                    bars.add(b);
+                                }
+                            }
+                            firestoreCallback.onCallback(bars);
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private interface FirestoreCallback {
+        void onCallback(List<BarItem> list);
     }
 
     @Override
