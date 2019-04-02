fb55dd69e44d90b25985e2cf494d76327409fd48
swmaryland <swmaryland@crimson.ua.edu>
2019-04-02 14:46:13 -0500

    setting up unit tests

---
+++ Bar_Buddy/app/src/test/java/com/example/bar_buddy/Activities/LoginActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.bar_buddy.Activities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+    @Test
+    public void LoginSuccessTest(){
+        assertThat();
+    }
+}
