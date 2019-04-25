298c653474864f009fe975bc222bd76634448010
ngleason7 <ngleason7@gmail.com>
2019-04-24 19:34:53 -0500

    Logo Fix

--- Bar_Buddy/app/src/main/java/com/example/bar_buddy/TabFragments/HomeTab.java
+++ Bar_Buddy/app/src/main/java/com/example/bar_buddy/TabFragments/HomeTab.java
@@ -19,6 +19,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.bar_buddy.Activities.MainActivity;
 import com.example.bar_buddy.Adapters.BarCardAdapter;
